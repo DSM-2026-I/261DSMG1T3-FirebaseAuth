ca5a1695bc36e436566eea8e08758471eefdca8f
Bryan Ochoa <bochoatrillo@gmail.com>
2026-05-03 00:04:25 -0500

    Agrega persistencia al username en la pantalla

--- app/src/main/java/com/example/firebaseauth2/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/example/firebaseauth2/presentation/home/HomeScreen.kt
@@ -8,13 +8,16 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun HomeScreen(userEmail: String?, onLogout: () -> Unit) {
-    val userName = userEmail?.substringBefore("@") ?: "User"
+    val userName = remember {
+        userEmail?.substringBefore("@") ?: "User"
+    }
 
     Column(
         modifier = Modifier.fillMaxSize(),
